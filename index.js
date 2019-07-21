f2ff2b93512dc4abb03bd8a91eb5352f3eef1af5
Sarah Farrell <sarahfarrell11@yahoo.co.uk>
2019-07-21 20:03:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+function Happyholidays(string)  {
+  if (string === "Happy holidays!") {
+  return string.to  ("Happy holidays!")
+}
+function happyHolidaysToSarah(string) { 
+  return string.to ("Happy Holidays, ${Sarah!") 
+}
+function happyHolidayToSarah(string) {
+  return string.to ("Happy ${Christmas}, ${Sarah}!")
+}
+function holidayCountdown(string) { 
+  return string.to ("It's ${11 days} until $ {Christmas!")
+  }
+}
+ 
+ 
