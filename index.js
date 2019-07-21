cdf2adda2e884d84c9bb402ade2110e96c64da11
Sarah Farrell <sarahfarrell11@yahoo.co.uk>
2019-07-21 20:20:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function happyHolidays()  {
   return "Happy holidays!";
 }
 function happyHolidaysToSarah() { 
-  return "Happy Holidays, ${Sarah!";
+  return "Happy Holidays, Sarah!";
 }
 function happyHolidayToSarah() {
-  return "Happy ${Christmas}, ${Sarah}!";
+  return "Happy Christmas, Sarah!";
 }
 function holidayCountdown(string) { 
   return "It's 20 days until Mother's Day!";
